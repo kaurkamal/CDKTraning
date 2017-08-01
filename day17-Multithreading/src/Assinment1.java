8d0151ad141c928154f6c69274d0cc54a9c04d2f
kaurkamal <kamaljeet19071995@gmail.com>
2017-08-01 14:49:03 +0530

    "Multithreading 1 aug assignment"

--- day17-Multithreading/src/Assinment1.java
+++ day17-Multithreading/src/Assinment1.java
@@ -8,21 +8,25 @@ public class Assinment1 {
         LinkedBlockingQueue<Integer> blockingQueue = new LinkedBlockingQueue<>();
         Producer producer = new Producer(blockingQueue);
         Consumer consumer = new Consumer(blockingQueue);
-        Thread thread1 = new Thread(()->{
+        Thread thread1 = new Thread(() -> {
             for (int i = 0; i < 500; i++) {
-            try {
-                blockingQueue.put((int)(Math.random() * 1000));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }}
+                try {
+                    blockingQueue.put((int) (Math.random() * 1000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         });
         thread1.start();
-        Thread thread2 = new Thread(()-> { while (true) {
-            try {
-                System.out.println(blockingQueue.take());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }}}
+        Thread thread2 = new Thread(() -> {
+            while (true) {
+                try {
+                    System.out.println(blockingQueue.take());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         );
         thread2.start();
 
